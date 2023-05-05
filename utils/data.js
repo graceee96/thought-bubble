75307ded77afe618356656a4737ee982d7457cd3
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-05 00:57:54 -0700

    start writng data to seed in database

---
+++ utils/data.js
@@ -0,0 +1,23 @@
+const users = [
+    'picture_it_sophia',
+    'bigmean_dorothy',
+    'blanche_forever45',
+    'rose_of_stolaf',
+    'dean_of_greendale',
+    'troy.troy.troy',
+    'el_tigre_chino',
+    'the_real_abed',
+    'annie.edison'
+];
+
+const emails = [
+    'sophiapetrillo@goldengirls.com',
+    'd.zbornak@goldengirls.com',
+    'blanche45@goldengirls.com',
+    'rose.nylund@goldengirls.com',
+    'pelton.in.charge@greendale.edu',
+    'troy.barnes01@greendale.edu',
+    'ben.chang10@greendale.edu',
+    'abed.nadir25@greendale.edu',
+    'annie.edison20@greendale.edu'
+];
---
+++ utils/seed.js
@@ -0,0 +1,11 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+})
