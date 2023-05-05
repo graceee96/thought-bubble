556a2fa1e4343df177e70b8906133902e8285ba9
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-04 22:54:12 -0700

    fix index.js

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+            match: {$regex: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/},
         },
         thoughts: [Thoughts],
         friends: [users]
