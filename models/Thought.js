ea49e2ae8fb78242bcfa3d4c45d3aa792a74cf34
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-06 03:06:11 -0700

    write thoughtData

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require('mongoose');
 const reactions = require('./Reaction');
 const formatDate = require('../utils/helpers/formatDate');
-const formatDate = require('../utils/helpers/formatDate')
 
 const thoughtSchema = new Schema(
     {
